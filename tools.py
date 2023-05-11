3f2ec1f237a8c05a2f4e410698050a7dbac17769
v <xultezz@gmail.com>
2023-05-11 17:07:17 -0300

    construimos a função que gera circuitos para dim(rho) = 2,4,8,16. 2**n

--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+from qiskit import QuantumCircuit, Aer, execute
 import numpy as np
 import math
 from scipy.linalg import lapack
@@ -18,10 +19,12 @@ def get_abs_lambda(rho):
     return psi, u
 
 
-def get_psi_circuit(psi):
-    n_qubits = np.shape(psi)[0]-1
+def get_psi_circuit(psi, rho):
+    n_qubits = int(np.ceil(np.log2(np.shape(rho)[0])))
+    d = int(2*n_qubits)
     print(n_qubits)
-    qr = QuantumRegister(n_qubits)
+
+    qr = QuantumRegister(d)
     cr = ClassicalRegister(n_qubits)
     print(psi)
     for i in psi:
@@ -30,21 +33,46 @@ def get_psi_circuit(psi):
     if n_qubits == 1:
         lista = [qr[0]]
     else:
-        lista = [i for i in qr]#[,qr[1]]
+        lista = []
+        for i in range(n_qubits):
+            #lista = [i for i in qr]#[:-1]#[,qr[1]]
+            lista.append(qr[i])
+        lista2 = [qr[0],qr[1]]
     print('lista',lista)
+    print(len(qr))
+    print(qr)
+    print('lista2',lista2)
     #print('qr',[[qr[0], qr[1], qr[2], qr[3], qr[4], qr[5]]])
     #sys.exit()
     #sys.exit()
     qc = QuantumCircuit(qr,cr, name='qc_initialize')
+
     qc.initialize(psi, lista)
+    for i in range(int(n_qubits)):
+            qc.cx(i, int(n_qubits)+i)
     return qc
+
+def douglis(n):
+    n = 3
+    qubits = 2*n
+    #qr = QuantumRegister(qubits)
+    qc = QuantumCircuit(qubits, qubits, name = 'mistura')
+
+    def qc_CNOTS_mistura():
+    #  CNOT application for each pair of qubits
+        for i in range(n):
+            qc.cx(i, n+i)
+       # circuito.cx(n+i, i)
+        return qc
+    qc_CNOTS_mistura=qc_CNOTS_mistura()
+    qc_CNOTS_mistura.draw('mpl')
 
 
 if __name__ == "__main__":
     #for i in range(1):
     #    rho = rdm_ginibre(2)
     #    print(get_abs_lambda(rho))
-    rho = rdm_ginibre(4)
+    rho = rdm_ginibre(16)
     psi, u = get_abs_lambda(rho)
-    qc = get_psi_circuit(psi)
+    qc = get_psi_circuit(psi, rho)
     print(qc)
